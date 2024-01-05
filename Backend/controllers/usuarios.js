81424671cab57bbcd538826dd59568b729b66f5b
Eri <erickaponce92@gmail.com>
2024-01-04 22:17:33 -0500

    try validations

--- Backend/controllers/usuarios.js
+++ Backend/controllers/usuarios.js
@@ -1,3 +1,4 @@
+const {response} = require('express');
 const Usuario = require('../models/usuario');
 
 const getUsuarios = async(req, res) => {
@@ -13,14 +14,33 @@ const getUsuarios = async(req, res) => {
 const crearUsuario = async(req, res) => {
 
     const {email, password, nombre} = req.body;
-    const usuario = new Usuario(req.body);
 
-    await usuario.save();
+    try {
+        const existeEmail = await Usuario.findOne({email});
 
-    res.json({
-        ok: true,
-        usuario
-    })
+        if (existeEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado'
+            })
+        }
+
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+    
+        res.json({
+            ok: true,
+            usuario
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+
 }
 
 module.exports = {
