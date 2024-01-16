f966d185ab89b5f19cb004b1aadf4760c23a98f2
Eri <erickaponce92@gmail.com>
2024-01-16 12:22:11 -0500

    add search by collection

--- Backend/routes/busqueda.js
+++ Backend/routes/busqueda.js
@@ -1,13 +1,14 @@
 /* 
-    ruta: '/api/todo/:busqueda'
+    ruta: '/api/todo/'
 */
 
 const { Router } = require('express');
-const { getTodo } = require('../controllers/busqueda');
+const { getTodo, getDocumentosColeccion } = require('../controllers/busqueda');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/:busqueda', validarJWT, getTodo);
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion);
 
 module.exports = router;
