d48cdb6acbc480521e7444c041dc9316c6c55abc
Eri <erickaponce92@gmail.com>
2024-01-25 13:08:01 -0500

    add logic to the doctor and hospital in the put and delete routes

--- Backend/controllers/hospitales.js
+++ Backend/controllers/hospitales.js
@@ -40,22 +40,73 @@ const crearHospital = async(req, res = response) => {
 
 }
 
-const actualizarHospital = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarHospital'
-    })
+const actualizarHospital = async(req, res = response) => {
+
+    const id  = req.params.id;
+    const uid = req.uid
+
+    try {
+
+        const hospital = await Hospital.findById( id );
+
+        if ( !hospital ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Hospital no encontrado',
+            });
+        }
+        
+        const cambiosHospital = {
+            ...req.body,
+            usuario: uid
+        }
+
+        const hospitalActualizado = await Hospital.findByIdAndUpdate( id, cambiosHospital, { new: true });
+
+        res.json({
+            ok: true,
+            hospital: hospitalActualizado
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
 }
 
-const eliminarHospital = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarHospital'
-    })
+const eliminarHospital = async(req, res = response) => {
+    const id  = req.params.id;
+
+    try {
+
+        const hospital = await Hospital.findById( id );
+
+        if ( !hospital ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Hospital no encontrado',
+            });
+        }
+
+        await Hospital.findByIdAndDelete( id );
+
+        res.json({
+            ok: true,
+            msg: 'Hospital Eliminado'
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
-
-
-
 
 
 module.exports={
