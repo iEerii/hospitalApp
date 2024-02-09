23d92a252ce3e1f9759cc30d697aa8092ab53a3c
JesusEmmanuelPonce <j.emmanuel.ponce.b@gmail.com>
2024-02-09 12:35:57 -0600

    feat: se agregó 404 component

--- frontend/src/app/pages/nopagefound/nopagefound.component.ts
+++ frontend/src/app/pages/nopagefound/nopagefound.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
   styleUrl: './nopagefound.component.css'
 })
-export class NopagefoundComponent {
+export class NopagefoundComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  year = new Date().getFullYear()
 }
