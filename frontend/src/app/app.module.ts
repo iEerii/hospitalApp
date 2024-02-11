03c9cec7591c763bbc7058b24ed55017e4767d90
JesusEmmanuelPonce <j.emmanuel.ponce.b@gmail.com>
2024-02-10 21:46:14 -0600

    feat: add shared module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
@@ -14,10 +10,6 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    PagesComponent,
     NopagefoundComponent
   ],
   imports: [
--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { GraficaComponent } from './grafica/grafica.component';
+import { SharedModule } from '../shared/shared.module';
+import { PagesComponent } from './pages.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     GraficaComponent,
+    PagesComponent,
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     GraficaComponent,
+    PagesComponent,
   ],
   imports: [
-    CommonModule
+    RouterOutlet,
+    CommonModule,
+    SharedModule,
   ]
 })
 export class PagesModule { }
